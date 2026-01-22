39c48661cea094cd2db2a1f34ec93d6bf271bf9b
Sophia <seloreshernandez@gmail.com>
2026-01-22 15:14:25 -0800

    Added basic setup for iframe

---
+++ ucsd-news-tamagotchi.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin-Name: ucsd news tamagotchi
+ * Description: Tamagotchi for UCSD: The Guardian webpage
+ * Authors:
+ * Version: 1.0.0
+ */
+
+function tamagotchi_iframe() {
+    ?>
+    <div id="tamagotchi-container">
+        <iframe
+            src ="insert iframe link"
+            width = "100"
+            height = "100"
+            frameborder = "0"
+            scrolling = "no"
+        ></iframe>
+    </div>
+    <?php
+}
+
