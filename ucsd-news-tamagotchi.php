80beba199f973feff31a3f26b863bf3a677ef33f
Sophia <seloreshernandez@gmail.com>
2026-01-22 17:46:33 -0800

    Cleanup to php file and adjusted css for smaller screens

--- ucsd-news-tamagotchi.php
+++ ucsd-news-tamagotchi.php
@@ -1,27 +1,32 @@
 <?php
 
 /**
- * Plugin-Name: ucsd news tamagotchi
+ * Plugin Name: ucsd news tamagotchi
  * Description: Tamagotchi for UCSD: The Guardian webpage
- * Authors:
+ * Authors: Samuel and Sophia
  * Version: 1.0.0
  */
+
+//Prevents direct access
+if(!defined('ABSPATH')) {
+    exit;
+}
 
 // Add the iframe 
 function tamagotchi_iframe() {
     ?>
     <div id="tamagotchi">
         <iframe
-            src ="insert iframe link"
+            src = "iframe link"
             width = "500"
             height = "500"
-            style = "border: none";
-            scrolling = "no";
+            style = "border: none;"
+            scrolling = "no"
         ></iframe>
     </div>
     <?php
 }
 
 //Wordpress function allowing it to appear on page
-add_action('wp-footer', 'tamagotchi_iframe');
+add_action('wp_footer', 'tamagotchi_iframe');
 
